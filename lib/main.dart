854aba9a0a13291b8865e7b69342b44556180f5a
xiedong <zx381010>
2018-09-25 15:41:57 +0800

    提交手势监听代码

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,39 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-
-//https://blog.csdn.net/u011045726/article/details/79583423
-//https://blog.csdn.net/hekaiyou/article/details/72853738
 void main() {
-  runApp(new MyApp());
+  runApp(new MaterialApp(
+    title: 'GestureDetector',
+    home: new MyButton(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+/**
+ * 手势监听
+ */
+class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      home: new MyHomePage(title: '应用程序首页'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  bool _result = false;
-
-  Future<Null> _openNewPage() async {
-    bool value = await Navigator.of(context).push(new MaterialPageRoute<bool>(
-        builder: (BuildContext context) {
-          return new Center(
-            child: new GestureDetector(
-              child: new Text("确定"),
-              onTap: () { Navigator.of(context).pop(true); },
-            ),
-          );
-        }
-    ));
-
-    setState(() {
-      _result = value;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: new Text(
-          '用户当前选择为 $_result',
+    return new GestureDetector(
+      onTap: () {
+        print("Button is click。。。。");
+      },
+      onDoubleTap: () {
+        print("Button is double click。。。。");
+      },
+      onLongPress: () {
+        print("Button is long press。。。。");
+      },
+      child: new Container(
+        height: 36.0,
+        padding: const EdgeInsets.all(80.0),
+        margin: const EdgeInsets.symmetric(vertical: 8.0),
+        decoration: new BoxDecoration(
+          borderRadius: new BorderRadius.circular(10.0),
+          color: Colors.lightBlue,
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _openNewPage,
-        child: new Icon(Icons.open_in_new),
+        child: new Center(
+          child: new Text('点击监听'),
+        ),
       ),
     );
   }
