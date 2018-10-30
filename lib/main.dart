0d694a09aeff0952ce5b712501c112c04e58f126
xiedong <zx381010>
2018-10-30 11:25:59 +0800

    添加gridlist

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,61 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MaterialApp(
-    title: "dsf",
-//    home: new MyApp());
+  runApp(
+    new MaterialApp(
+      title: 'd',
+      home: new MyApp(),
+    ),
+  );
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: new IconButton(
+            icon: new Icon(Icons.ac_unit), tooltip: "dfsaf", onPressed: null),
+        title: new Text("title"),
+        actions: <Widget>[
+          new IconButton(
+              icon: new Icon(Icons.title), tooltip: "fsdafsa", onPressed: null)
+        ],
+      ),
+      body: new Stack(
+        children: <Widget>[
+          new Align(
+            alignment: new FractionalOffset(0.5, 0.5),
+            child: new MyButton(),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: () {
+        print("button is click ...");
+      },
+      child: new Container(
+        height: 36.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+        decoration: new BoxDecoration(
+          borderRadius: new BorderRadius.circular(15.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: new Center(
+          child: new Text(
+            'Flutter',
+            style: new TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
