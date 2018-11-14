41825a564f754ec468b8b4b5c34b51cd1f2cd894
xiedong <zx381010>
2018-11-14 10:16:41 +0800

    添加进度条代码

---
+++ lib/ToastAndDialog/CicularProgressIndicatorDemo.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(home: new LinearProgressIndicatorDemo()));
+}
+
+class LinearProgressIndicatorDemo extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new MyProgressIndicatorState();
+}
+
+class MyProgressIndicatorState extends State<LinearProgressIndicatorDemo> {
+  double _currentIndex = 0.1;
+
+  /**
+   * 更新进度条当前值
+   */
+  void changeCurrentIdex() {
+    setState(() {
+      _currentIndex += 0.1;
+      if (_currentIndex >= 1.0) {
+        _currentIndex = 0.1;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("条形进度条"),
+        ),
+        body: new Column(
+          children: <Widget>[
+            new CircularProgressIndicator(
+              backgroundColor: Colors.white,
+              value: _currentIndex, //(在不指定value的情况下，进度条一直滚动)
+              strokeWidth: 10.0,
+            ),
+            new Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: new RaisedButton(
+                onPressed: changeCurrentIdex,
+                child: new Text("改变进度"),
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+//const CircularProgressIndicator({
+//Key key,
+//double value,
+//Color backgroundColor,
+//Animation<Color> valueColor,
+//this.strokeWidth: 4.0,
+//})
