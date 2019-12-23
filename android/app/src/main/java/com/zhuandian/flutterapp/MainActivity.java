346bfbd05c0331fe47460d871930edcb8b64e6ab
xiedong <xieguodong@chinanews.com.cn>
2019-12-23 15:03:13 +0800

    原生平台增加加法运算功能给flutter端调用

--- android/app/src/main/java/com/zhuandian/flutterapp/MainActivity.java
+++ android/app/src/main/java/com/zhuandian/flutterapp/MainActivity.java
@@ -13,6 +13,7 @@ import io.flutter.plugins.GeneratedPluginRegistrant;
 public class MainActivity extends FlutterActivity {
     private static final String CHANNEL = "com.zhuandian.flutter/android";
     private static final String CHANNEL_TYPE_SHOW_TOAST = "showToast";
+    private static final String CHANNEL_TYPE_NUMBER_ADD = "numberAdd"; //简单加法计算，并返回两个数的和
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +21,22 @@ public class MainActivity extends FlutterActivity {
 //        setContentView(R.layout.activity_main);
         GeneratedPluginRegistrant.registerWith(this);
 
+        //接受fltuter端传递过来的方法，并做出响应逻辑处理
         new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(new MethodChannel.MethodCallHandler() {
             @Override
             public void onMethodCall(MethodCall call, MethodChannel.Result result) {
                 System.out.println(call.method);
-                if (call.method.equals("showToast")) {
+                if (call.method.equals(CHANNEL_TYPE_SHOW_TOAST)) {
                     if (call.hasArgument("msg") && !TextUtils.isEmpty(call.argument("msg").toString())) {
                         Toast.makeText(MainActivity.this, call.argument("msg").toString(), Toast.LENGTH_SHORT).show();
                     } else {
                         Toast.makeText(MainActivity.this, "toast text must not null", Toast.LENGTH_SHORT).show();
                     }
-                }else if (call.method.equals("new_page")){
+                } else if (call.method.equals(CHANNEL_TYPE_NUMBER_ADD)) {
+                    int number1 = call.argument("number1");
+                    int number2 = call.argument("number2");
+                    result.success(number1 + number2); //返回两个数相加后的值
+                } else if (call.method.equals("new_page")) {
                     startActivity(new Intent(MainActivity.this, SecondActivity.class));
                 }
             }
--- lib/pages/platform/android_platform_page.dart
+++ lib/pages/platform/android_platform_page.dart
@@ -18,6 +18,7 @@ class PageState extends State<AndroidPlatformPage> {
 
   static final String CHANNEL = "com.zhuandian.flutter/android";
   static final String SHOW_NATIVE_TOAST = "showToast"; //原生android平台定义的供flutter端唤起的方法名
+  static final String NATIVE_METHOD_ADD = "numberAdd"; //原生android平台定义的供flutter端唤起的方法名
 
   static final MethodChannel _MethodChannel = MethodChannel(CHANNEL); //平台交互通道
   @override
@@ -32,9 +33,17 @@ class PageState extends State<AndroidPlatformPage> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           RaisedButton(
+            color: Colors.orangeAccent,
             child: Text("调用原生平台Toast"),
             onPressed: (){
               showNativeToast("flutter调用原生android的Toast");
+            },
+          ),
+          RaisedButton(
+            color: Colors.orangeAccent,
+            child: Text("计算两个数的和"),
+            onPressed: (){
+              getNumberResult(25,36);
             },
           )
         ],
@@ -44,5 +53,18 @@ class PageState extends State<AndroidPlatformPage> {
 
   void showNativeToast(String content) {
     _MethodChannel.invokeMethod(SHOW_NATIVE_TOAST,{"msg":content});
+  }
+
+  /**
+   * 调用平台方法计算两个数的和，并调用原生toast打印出结果
+   */
+  void getNumberResult(int i, int j) async {
+    Map<String,dynamic> map = {
+      "number1":12,
+      "number2":43
+    };
+    int result = await _MethodChannel.invokeMethod(NATIVE_METHOD_ADD,map);
+
+    _MethodChannel.invokeMethod(SHOW_NATIVE_TOAST,{"msg":"12+43= $result"});
   }
 }
