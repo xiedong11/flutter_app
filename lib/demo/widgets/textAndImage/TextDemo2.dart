45af37fe80622a7950ad87acdd02f8224e803953
xiedong <xieguodong@chinanews.com.cn>
2020-09-22 13:19:51 +0800

    添加textdemo2

---
+++ lib/demo/widgets/textAndImage/TextDemo2.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+    theme: ThemeData(
+      primaryColor: Colors.red,
+      accentColor: Colors.redAccent,
+    ),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Text")),
+        body: Text(
+          "Hello Flutter,Hello Flutter,Hello Flutter,"
+          "Hello Flutter,Hello Flutter,"
+          "Hello Flutter,Hello Flutter,"
+          "Hello Flutter,Hello Flutter,"
+          "Hello Flutter,Hello Flutter,",
+          style: TextStyle(
+              color: Colors.blue,
+//            backgroundColor: Colors.black
+              fontSize: 19,
+              fontWeight: FontWeight.bold,
+//            fontStyle: FontStyle.italic,
+              letterSpacing: 2.0,
+              wordSpacing: 10.0,
+          ),
+        ));
+  }
+}
