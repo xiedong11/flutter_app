ae7d119d3290d05415749dbb0e31a726fb80220b
xiedong <xieguodong@chinanews.com.cn>
2021-04-25 15:49:12 +0800

    暂存文件

---
+++ lib/demo/widgets/image/ImageDemo.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/**
+ * desc:
+ * author: xiedong
+ * date: 2/9/21
+ **/
+
+void main() {
+  runApp(MaterialApp(
+    home: ImageDemo(),
+  ));
+}
+
+class ImageDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ImageDemo"),
+      ),
+      // body: Image(""),
+    );
+  }
+}
