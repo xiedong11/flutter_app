6be5303ce458ec994e104a00d2bba7402166b0d0
xiedong <xiedong11@aliyun.com>
2024-01-15 11:23:24 +0800

    Scaffold.of 找不到上下文问题 可以在组件外部包裹一层 Builder组件来提供上下文参数

--- lib/pages/simpleWidget/TextFieldPage.dart
+++ lib/pages/simpleWidget/TextFieldPage.dart
@@ -57,6 +57,16 @@ class MyAppState extends State<TextFieldPage> {
 //                  hintText: "fdsfdss",
                   helperText: "注册名字"),
             ),
+            /**
+             * 在 Flutter 中 Builder 组件的作用是解决 Scaffold.of 找不到上下文问题的解决文案
+             *
+             * 1.Builder 控件的作用，我的理解是在于重新提供一个新的子 context ，通过新的 context 关联到相关祖先从而达到正常操作的目的。
+             *  2.同样的对于路由跳转 Navigator.of(context)【注：Navigator 是由 MaterialApp 提供的】 等类似的问题，采用的都是类似的原理，只要搞懂了其中一个，其他的都不在话下！
+             *
+             *
+             * TODO tips 20240115
+             *  新版本的Flutter采用ScaffoldMessenger.of的形式，不存在此问题？？？
+             */
             new Builder(builder: (BuildContext context) {
               return new ElevatedButton(
                 onPressed: () {
@@ -71,6 +81,9 @@ class MyAppState extends State<TextFieldPage> {
                     onTextClear(); //情况输入内容，让用户重新输入
                   }
                 },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue)
+                ),
                 // color: Colors.blue,
                 // highlightColor: Colors.deepPurple,
                 // disabledColor: Colors.cyan,
